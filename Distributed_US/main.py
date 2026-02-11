ef92e45f2f6cb9458d53a4578bd3c88767782bd5
CLampard-Y <Petwosagannn@163.com>
2026-02-11 23:54:15 +0800

    feat(crawler): implement API fetching and database connection logic
    - Add fetch_prices() with 3-retry mechanism and 10s timeout
    - Add save_to_db() skeleton for PostgreSQL data persistence
    - Enhance inline documentation for HTTP status codes and retry logic
    - Implement resilient design for HK peak hour network instability

--- Distributed_US/main.py
+++ Distributed_US/main.py
@@ -11,7 +11,7 @@ from datetime import datetime
 import pytz
 
 # --------------------------------------------
-# Read server information from .env
+# 1. Read server & API information from .env
 # Prevent hard-coded code
 # --------------------------------------------
 # US Server information
@@ -28,6 +28,46 @@ SOURCE_REGION = os.getenv("SOURCE_REGION", "UNKNOWN")
 API_URL = os.getenv("API_URL","FULL_URL")
 API_PROVIDER = os.getenv("API_PROVIDER", "UNKONWN")
 
+# --------------------------------------------
+# 2. Fetch data from API
+# --------------------------------------------
+def fetch_prices():
+    max_retries = 3
+    # i = 0, 1, 2
+    for i in range(max_retries):
+        try:
+            print(f"[{SOURCE_REGION}] Attempt {i+1}: Fetching data from [{API_PROVIDER}]...")
+            
+            # Send GET request to API_URL
+            # Attention: requests.get() wont raise exception if API is down
+            response = requests.get(API_URL, timeout=10)    # timeout=10 sec
 
+            # Ask for JSON response:
+            # 200 OK: Successful request
+            # 404 Not Found: API not found -> exception
+            # 500 Internal Server Error: API error -> exception
+            # 503 Service Unavailable: API error -> exception
+            response.raise_for_status()
+            return response.json()
+        
+        # print error message and sleep
+        except Exception as e:
+            print(f"Error fetching data: {e}")
+            time.sleep(2)
+    raise Exception("Failed to fetch data after retries")
 
+# --------------------------------------------
+# 3. Write to remote US database
+# --------------------------------------------
+def save_to_db(data):
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS
+        )
+        cur = conn.cursor()
+        
+        
 
